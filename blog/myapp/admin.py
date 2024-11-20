2310249def7268941b3b81b80dba2e1e86ae0bc7
Sadhon Kumar Dev <skdsadhon@gmail.com>
2024-11-20 20:01:07 +0600

    skdtest1

--- blog/myapp/admin.py
+++ blog/myapp/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import TodoItem
 
-# Register your models here.
+# Register your models here. skd
 admin.site.register(TodoItem)
 
 # username= SadhonKD pass= skdSKD#@
